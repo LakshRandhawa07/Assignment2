60a44b15988b0dcc8a8da2652b83c3461d3ef153
LakshRandhawa07 <randhawalakshdeep07@gmail.com>
2023-02-09 15:34:07 -0500

    second commit

---
+++ src/Loopingvalueandadding.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loopingvalueandadding {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int sum=0;
+        while(0==0){
+            System.out.printf("please enter a number :");
+            int x=sc.nextInt();
+if(x<0){break;}
+sum=sum+x;}
+            System.out.println("The sum of the entered numbers are :" +sum);
+
+
+    }
+}
